6d3645e54cf8aa032d14c0f93d641f312824bfd4
Anshul Khandelwal <kanshul45@gmail.com>
2020-07-03 12:50:14 +0530

    Added Firebase And google services

--- app/src/main/java/com/candywal/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/candywal/whatsappclone/MainActivity.kt
@@ -2,10 +2,26 @@ package com.candywal.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //test
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+    // Checks if the user is logged in already
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+       // updateUI(currentUser)
     }
 }
