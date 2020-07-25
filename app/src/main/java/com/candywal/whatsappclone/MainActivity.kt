a69d184b1283155b880d4237e2d852945f1bd619
Anshul Khandelwal <kanshul45@gmail.com>
2020-07-25 12:51:36 +0530

    Started work on the Registration screen

--- app/src/main/java/com/candywal/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/candywal/whatsappclone/MainActivity.kt
@@ -2,9 +2,12 @@ package com.candywal.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,12 +19,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main) //test
         // Initialize Firebase Auth
         auth = Firebase.auth
-    }
+        val regName = regName.text.toString()
+        val regNum = regPhone.text.toString()
+
+        Log.d("MainActivity", "Name is $regName")
+        Log.d("MainActivity", "Num is $regNum")
+       }
     // Checks if the user is logged in already
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
-       // updateUI(currentUser)
+        //updateUI(currentUser)
     }
 }
